058f6aadb389ddac2e3a39e2f35c31db082f5711
kariah <41475006+kariah@users.noreply.github.com>
2021-04-04 16:55:06 +0300

    Add files via upload

---
+++ osa1/kurssitiedot/1.1/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+ 
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+ 
+const Content = ({parts}) => {
+  return (
+    <div>  {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}</div>
+  )
+}
+
+
+const Total =  ({parts}) => {
+  const sumOfExercises = parts.reduce(
+    (prevValue, currentValue) => prevValue + currentValue.exercises,
+    0
+  );
+  return <p>Number of exercises: {sumOfExercises}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const parts = [
+    { id: 1, name: 'Fundamentals of React', exercises: 10 },
+    { id: 2, name: 'Using props to pass data', exercises: 7 },
+    { id: 3, name: 'State of a component', exercises: 14 },
+  ]
+ 
+
+  return (
+    <>
+      <Header course={course} /> 
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </>
+  )
+} 
+
+export default App
