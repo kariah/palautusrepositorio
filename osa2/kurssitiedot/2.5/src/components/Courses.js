b82fac928bf8314c60faad7153dc84ee38cd6d6a
kariah <41475006+kariah@users.noreply.github.com>
2021-04-09 18:25:20 +0300

    Add files via upload

---
+++ osa2/kurssitiedot/2.5/src/components/Courses.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Courses = (props) => {
+    const courses = props.courses
+
+    return (
+        <div>
+            <div>
+                 {courses.map(course => <Course key={course.id} course={course} />)} 
+            </div>
+        </div>
+    )
+}
+
+
+const Course = (props) => {
+    const name = props.course.name
+    const parts = props.course.parts
+
+    return (
+        <div>
+            <Header name={name} />
+            <Content parts={parts} />
+            <Total parts={parts} />
+        </div>
+    )
+}
+
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1> {props.name}</h1>
+        </div>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => <Part key={part.id} part={part} />)}
+        </div>
+    )
+}
+
+const Part = ({ part }) => (
+    <p>{part.name} {part.exercises}</p>
+)
+
+
+const Total = ({ parts }) => {
+    const sum = parts.map(part => part.exercises).reduce((sum, val) => sum + val, 0);
+    return <p><b>total of exercises: {sum}</b></p>
+}
+
+export default Courses
