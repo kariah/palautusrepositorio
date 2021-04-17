fa44ee8e01514a5277f56e329bbcaf87860d89f2
kariah <41475006+kariah@users.noreply.github.com>
2021-04-17 13:44:13 +0300

    Update persons.js

--- osa2/puhelinluettelo/2.17/src/services/persons.js
+++ osa2/puhelinluettelo/2.17/src/services/persons.js
@@ -17,14 +17,7 @@ const getAll = () => {
 const create = newObject => {
     const request = axios.post(baseUrl, newObject)
     return request.then(response => response.data)
-} 
-
-
-//const update = (id, newObject) => {
-//    const request = axios.put(`${baseUrl}/${id}`, newObject)
-//    return request.then(response => response.data)
-//}
-
+}  
 
 const remove = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`) 
