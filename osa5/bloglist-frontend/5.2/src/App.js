8a8d2395c326c68129599d8e92f6e9f298954362
kariah <41475006+kariah@users.noreply.github.com>
2021-05-14 12:17:59 +0300

    Add files via upload

---
+++ osa5/bloglist-frontend/5.2/src/App.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login' 
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('khtest5') 
+  const [password, setPassword] = useState('passu5') 
+  const [user, setUser] = useState(null)
+
+  
+  useEffect(() => {  
+    blogService
+      .getAll()
+      .then(blogs => {
+      setBlogs(blogs)
+    })
+  }, []) 
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+
+      blogService
+      .getAll()
+      .then(blogs => {
+        setBlogs(blogs)
+    })
+      
+    }
+  }, [])  
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      }) 
+ 
+      blogService.setToken(user.token)
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+ 
+      setUser(user)
+      setUsername('')
+      setPassword('') 
+      
+      blogService
+        .getAll()
+        .then(blogs => {
+          setBlogs(blogs)
+      })
+
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser') 
+    window.location.href = '/'
+  }
+
+  if (user === null) {
+  return (
+    <div> 
+      <Notification message={errorMessage} />
+      
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form> 
+    </div>
+  )
+    }
+
+  return ( 
+    <div>
+      <h2>blogs</h2>
+      <div>
+        <button onClick={handleLogout}>logout</button>
+      </div>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+}
+
+export default App
