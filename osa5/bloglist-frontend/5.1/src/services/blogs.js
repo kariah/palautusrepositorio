26c99dd20d8b7df2c8412e4286d21bbda0ae6c54
kariah <41475006+kariah@users.noreply.github.com>
2021-05-14 11:45:33 +0300

    Update blogs.js

--- osa5/bloglist-frontend/5.1/src/services/blogs.js
+++ osa5/bloglist-frontend/5.1/src/services/blogs.js
@@ -16,22 +16,7 @@ const getAll = async () => {
 const setToken = newToken => {
   token = `bearer ${newToken}`
 }
-
-// const create = async newObject => {
-//   const config = {
-//     headers: { Authorization: token },
-//   }
-
-//   const response = await axios.post(baseUrl, newObject, config)
-//   return response.data
-// }
-
-// const update = async (id, newObject) => {
-//   const request = axios.put(`${baseUrl}/${id}`, newObject)
-//   const response = await request
-//   return response.data
-// }
-
+ 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, setToken, 
 }
