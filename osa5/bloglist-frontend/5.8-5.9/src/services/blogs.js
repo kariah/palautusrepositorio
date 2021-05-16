1c1cfa85427e7b50c759da00bcdac97cb2a0b99a
kariah <41475006+kariah@users.noreply.github.com>
2021-05-16 14:22:24 +0300

    Add files via upload

---
+++ osa5/bloglist-frontend/5.8-5.9/src/services/blogs.js
@@ -0,0 +1,40 @@
+import axios from 'axios' 
+const baseUrl = 'api/blogs' 
+let token = null 
+
+const getAll = async () => { 
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.get(baseUrl, config) 
+  const response = await request  
+   
+  return response.data 
+} 
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  } 
+ 
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = async (id, updateObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+ 
+  const request = axios.put(`${baseUrl}/${id}`, updateObject, config)
+  const response = await request
+  return response.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, setToken, create, update }
