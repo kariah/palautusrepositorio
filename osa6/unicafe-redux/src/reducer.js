d2bc82d4155485fa46dba54afa9345ff4ceb8310
kariah <41475006+kariah@users.noreply.github.com>
2021-05-31 10:01:21 +0300

    Add files via upload

---
+++ osa6/unicafe-redux/src/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  let changedState
+  switch (action.type) { 
+    case 'GOOD': 
+       changedState = { 
+        ...state, 
+        good: state.good + 1
+      }
+      return changedState 
+    case 'OK':
+      changedState = { 
+        ...state, 
+        ok: state.ok + 1
+      }
+      return changedState 
+    case 'BAD':
+      changedState = { 
+        ...state, 
+        bad: state.bad + 1
+      }
+      return changedState 
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
