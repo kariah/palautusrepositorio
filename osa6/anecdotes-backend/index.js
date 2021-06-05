d789efdd0132b256dc864c0bf2aaef5e22a693a3
kariah <41475006+kariah@users.noreply.github.com>
2021-06-05 14:28:11 +0300

    Add files via upload

--- osa6/anecdotes-backend/index.js
+++ osa6/anecdotes-backend/index.js
@@ -5,41 +5,7 @@ const app = express()
 app.use(express.json()) 
 app.use(cors())
 
-let anecdotes = 
-//{
-//     "anecdotes":[
-//       {
-//         "content": "If it hurts, do it more often",
-//         "id": "47145",
-//         "votes": 0
-//       },
-//       {
-//         "content": "Adding manpower to a late software project makes it later!",
-//         "id": "21149",
-//         "votes": 0
-//       },
-//       {
-//         "content": "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-//         "id": "69581",
-//         "votes": 0
-//       },
-//       {
-//         "content": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-//         "id": "36975",
-//         "votes": 0
-//       },
-//       {
-//         "content": "Premature optimization is the root of all evil.",
-//         "id": "25170",
-//         "votes": 0
-//       },
-//       {
-//         "content": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-//         "id": "98312",
-//         "votes": 0
-//       }
-//     ]
-//   }
+let anecdotes =  
  
     [{
         "content": "If it hurts, do it more often",
@@ -101,13 +67,41 @@ app.post('/api/anecdotes', (request, response) => {
     content: body.content, 
     id: generateId(),
     votes: 0
-  }
-
-  console.log(anecdote)
+  } 
 
   anecdotes = anecdotes.concat(anecdote)
 
   response.json(anecdote)
+})
+
+app.put('/:id', async (request, response) => {
+    const body = request.body
+  
+    //const id = Number(request.params.id)
+    const id = request.params.id
+ 
+    if (!body.votes) {
+        return response.status(400).json({ 
+          error: 'data missing' 
+        })
+    }
+
+    const anecdoteToChange = anecdotes.find(n => n.id === id)
+ 
+    const changedAnecdote = { 
+      ...anecdoteToChange, 
+      votes: body.votes
+    } 
+
+    let updatedAnecdotes = anecdotes
+      .map(anecdote =>
+      anecdote.id !== id ? anecdote : changedAnecdote 
+    ) 
+    anecdotes = updatedAnecdotes  
+ 
+    response.json({
+        changedAnecdote
+    })
 })
 
 // app.get('/api/notes/:id', (request, response) => {
