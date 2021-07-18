ead1eaa5090a9597b8bf140613b238e1204769ac
kariah <41475006+kariah@users.noreply.github.com>
2021-07-18 18:37:15 +0300

    Add files via upload

--- osa8/books-backend/index.js
+++ osa8/books-backend/index.js
@@ -118,6 +118,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]
     ): Book 
+    editAuthor( 
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `   
 
@@ -164,6 +168,16 @@ const resolvers = {
 
       return book
     },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+  
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
+    }  
   } 
 } 
  
