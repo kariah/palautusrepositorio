02e45f4e48e91792519c85a118b6ab8ef52c24b9
kariah <41475006+kariah@users.noreply.github.com>
2021-07-29 14:06:13 +0300

    Add files via upload

---
+++ osa9/typescript_exercises/bmiCalculator.ts
@@ -0,0 +1,60 @@
+type Result = string
+
+const calculateBmi = (height: number, weight: number) : Result => {
+   
+  const heightAsMeters = height / 100
+  const bmi = weight / heightAsMeters / heightAsMeters
+
+//   console.log('bmi ', bmi) 
+ 
+// Underweight (Severe thinness)	< 16.0	 
+// Underweight (Moderate thinness)	16.0 – 16.9	 
+// Underweight (Mild thinness)	17.0 – 18.4 
+// Normal range	18.5 – 24.9	 
+// Overweight (Pre-obese)	25.0 – 29.9 
+// Obese (Class I)	30.0 – 34.9	 
+// Obese (Class II)	35.0 – 39.9 
+// Obese (Class III)	≥ 40.0  
+
+   let bmiResultText = ''
+
+   switch (true) {
+    case (bmi <= 16): 
+        bmiResultText = 'Underweight (Severe thinness)'
+        break
+    case (bmi >= 16 && bmi < 17): 
+        bmiResultText = 'Underweight (Moderate thinness)'
+        break
+    case (bmi >= 17 && bmi < 18.5):
+        bmiResultText = 'Underweight (Mild thinness)'
+        break
+    case (bmi >= 18.5 && bmi < 25):
+        bmiResultText = 'Normal range (healthy weight)'
+        break
+    case (bmi >= 25 && bmi < 30):
+        bmiResultText = 'Overweight (Pre-obese)'
+        break
+    case (bmi >= 30 && bmi < 35):
+        bmiResultText = 'Obese (Class I)'
+        break
+    case (bmi >= 35 && bmi < 40):
+        bmiResultText = 'Obese (Class II)'
+        break
+    case (bmi >= 40):
+        bmiResultText = 'Obese (Class II)'
+        break
+    default: 
+        bmiResultText = 'Not Defined'
+        break
+    }
+   
+    return bmiResultText 
+}
+
+try {
+    console.log(calculateBmi(180, 74))
+
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
+ 
---
+++ osa9/typescript_exercises/exerciseCalculator.ts
@@ -0,0 +1,29 @@
+//Malli
+// type Operation = 'multiply' | 'add' | 'divide';
+
+// type Result = number;
+
+// const calculator = (a: number, b: number, op : Operation) : Result => {
+//   switch(op) {
+//     case 'multiply':
+//       return a * b;
+//     case 'divide':
+//       if( b === 0) throw new Error('Can\'t divide by 0!');
+//       return a / b;
+//     case 'add':
+//       return a + b;
+//     default:
+//       throw new Error('Operation is not multiply, add or divide!');
+//   }
+// }
+
+// try {
+//   console.log(calculator(1, 5 , 'divide'))
+// } catch (e) {
+//   console.log('Something went wrong, error message: ', e.message);
+// }
+
+
+//type Operation = 'multiply' | 'add' | 'divide';
+
+//type Result = string;
