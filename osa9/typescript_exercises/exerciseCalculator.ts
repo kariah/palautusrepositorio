4b047711c7142646826b118e3625e38974bb3aa6
kariah <41475006+kariah@users.noreply.github.com>
2021-07-29 17:35:31 +0300

    Add files via upload

--- osa9/typescript_exercises/exerciseCalculator.ts
+++ osa9/typescript_exercises/exerciseCalculator.ts
@@ -1,60 +1,70 @@
-// Malli
-// type Operation = 'multiply' | 'add' | 'divide';
- 
 interface ExcerciseCalculationResult {
     periodLength: number,
     trainingDays: number,
     success: boolean,
-    rating: string,
+    rating: number,
     ratingDescription: string,
     target: number,
     average: number
-  }
-  
-
-// type Result = TrainingResult
+  } 
 
 const calculateExercises  = (excerciseHoursInWeek:number[], target: number) : ExcerciseCalculationResult => { 
-     console.log(excerciseHoursInWeek)
-
      const sumOfTrainingHours =  excerciseHoursInWeek.reduce((a, b) => a + b, 0)
      const trainingDays = excerciseHoursInWeek.filter(x => x > 0).length
 
-     console.log('sumOfTrainingHours ', sumOfTrainingHours)
+     let averageTrainingHours = 0
+     if (trainingDays > 0)
+     {
+        averageTrainingHours = sumOfTrainingHours / trainingDays
+    }
+    else
+    {
+        averageTrainingHours = 0
+    }
 
-     let exerciseCalculationResult = {
+     let rating = 0
+     let ratingDescription = 'Not enough exercises!'  
+     let averageTrainingHoursComparedToTarget = averageTrainingHours - target  
+     
+    //  console.log('excerciseHoursInWeek ', excerciseHoursInWeek) 
+    //  console.log('averageTrainingHours ', averageTrainingHours) 
+    //  console.log('averageTrainingHoursComparedToTarget ', averageTrainingHoursComparedToTarget) 
+
+      if (averageTrainingHoursComparedToTarget < 0)
+      {
+        rating = 1,
+        ratingDescription = "Below target, could be better" 
+      } 
+      else if (averageTrainingHoursComparedToTarget >= 0 && averageTrainingHoursComparedToTarget < 1)
+      {
+        rating = 2,
+        ratingDescription = "Target or over the target, well done!" 
+      } 
+      else if (averageTrainingHoursComparedToTarget >= 1)
+      {
+        rating = 3 
+        ratingDescription = "A lot over the target, excellent!"
+      }  
+ 
+
+      let exerciseCalculationResult = {
         periodLength: excerciseHoursInWeek.length,
         trainingDays:  trainingDays,
         success: false,
-        rating: "ok",
-        ratingDescription: "ok2",
-        target: 1,
-        average: sumOfTrainingHours / trainingDays
-      }
-
-//   switch(op) {
-//     case 'multiply':
-//       return a * b;
-//     case 'divide':
-//       if( b === 0) throw new Error('Can\'t divide by 0!');
-//       return a / b;
-//     case 'add':
-//       return a + b;
-//     default:
-//       throw new Error('Operation is not multiply, add or divide!');
-//   }
-        return exerciseCalculationResult
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: target,
+        average: averageTrainingHours
+      }  
+ 
+    return exerciseCalculationResult
 }
 
 try { 
   const excerciseHoursInWeek:number[] = new Array(3, 0, 2, 4.5, 0, 3, 1)
 
-  console.log(calculateExercises(excerciseHoursInWeek, 2))
+  console.log(calculateExercises(excerciseHoursInWeek, 1))
 } catch (e) {
   console.log('Something went wrong, error message: ', e.message);
 }
-
-
-// type Operation = 'multiply' | 'add' | 'divide';
-
-// type Result = string;
+ 
