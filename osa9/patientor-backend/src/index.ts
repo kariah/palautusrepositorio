b841e397b05f178ec7724d430a7e9301d592d643
kariah <41475006+kariah@users.noreply.github.com>
2021-08-03 12:43:44 +0300

    Update index.ts

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -1,23 +1,3 @@
-// import express from 'express';
-
-// const app = express();
-// app.use(express.json());
-  
-// import cors from 'cors'; 
-// app.use(cors());
-
-// const PORT = 3001;
-
-// app.get('/api/ping', (_req, res) => {
-//   console.log('someone pinged here');
-//   res.send('pong');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
 import express from 'express';
 import diagnoseRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
