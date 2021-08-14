5dfcd6e05c18661ea2615aea6dca98bc60a2a77c
kariah <41475006+kariah@users.noreply.github.com>
2021-08-14 18:37:50 +0300

    Update index.tsx

--- osa9/patientor/src/AddPatientEntryModal/index.tsx
+++ osa9/patientor/src/AddPatientEntryModal/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, Segment } from 'semantic-ui-react';
-import AddPatientForm, { PatientEntryFormValues } from './AddHealthCheckEntryForm';
+import AddHealthCheckEntryForm, { PatientEntryFormValues } from './AddHealthCheckEntryForm';
 
 interface Props {
   modalOpen: boolean;
@@ -14,7 +14,7 @@ const AddHealthsCheckEntryModal = ({ modalOpen, onClose, onSubmit, error }: Prop
     <Modal.Header>Add a new entry</Modal.Header>
     <Modal.Content>
       {error && <Segment inverted color="red">{`Error: ${error}`}</Segment>}
-      <AddPatientForm onSubmit={onSubmit} onCancel={onClose} />
+      <AddHealthCheckEntryForm onSubmit={onSubmit} onCancel={onClose} />
     </Modal.Content>
   </Modal>
 );
